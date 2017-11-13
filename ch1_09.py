0dac693c18be6d13e9147bd907193eb63bc6cbc4
ia-svaudo <32363954+ia-svaudo@users.noreply.github.com>
2017-11-13 12:00:38 -0500

    should be done

--- ch1_09.py
+++ ch1_09.py
@@ -1,3 +1,10 @@
 # Name
 # Problem #
 # Chapter 1 Problem Set Part 1
+while(True):
+    a = int(input("Enter a number: "))
+    a = a + 2
+    a = a * 3
+    a = a - 6
+    a = a / 3
+    print(a)
--- ch1_10.py
+++ ch1_10.py
@@ -1,3 +1,10 @@
 # Name
 # Problem #
 # Chapter 1 Problem Set Part 1
+
+num_wifes = 7**1
+num_sack = 7**2
+num_cat = 7**3
+num_kittens = 7**4
+
+print(num_wifes + num_sack + num_cat + num_kittens)
