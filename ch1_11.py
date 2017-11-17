36744a30c854c9907a8c3b93b7971a1d64434260
ia-wbaker <32363954+ia-wbaker@users.noreply.github.com>
2017-11-17 15:41:56 -0500

    gafgafd
    dafsfsdfadf

---
+++ ch1_11.py
@@ -0,0 +1,7 @@
+# Name
+# Problem #
+# Chapter 1 Problem Set Part 1
+
+my_int = 5
+my_int = my_int + 3
+print(my_int)
--- ch1_Project_GreatLakes.py
+++ ch1_Project_GreatLakes.py
@@ -1,7 +1,6 @@
 # Name
 # Project: Great Lakes are how big?
-# Chapter 1 Problem Set Part 1
-a = 0.22 * 22810
-a = a/8080464.3
+# Chapter 1 Problem Set Part 1 
+a = 22810/8080464.3
 a = str(100000*a)
 print(a + "cm")
