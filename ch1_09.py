d2eff9225d644f767768b6a6acb1a8f07c54035e
ia-svaudo <32363954+ia-svaudo@users.noreply.github.com>
2017-11-17 15:12:48 -0500

    finished everything

--- ch1_09.py
+++ ch1_09.py
@@ -3,8 +3,4 @@
 # Chapter 1 Problem Set Part 1
 while(True):
     a = int(input("Enter a number: "))
-    a = a + 2
-    a = a * 3
-    a = a - 6
-    a = a / 3
-    print(a)
+    print((((a + 2) * 3) - 6) / 3)
--- ch1_17.py
+++ ch1_17.py
@@ -1,3 +1,13 @@
 # Name
 # Problem #
 # Chapter 1 Problem Set Part 1
+while(True):
+    a = input("Enter a number: ")
+    a_int = int(a)
+    a_float = float(a)
+    print(a, a_int, a_float)
+#If you input something that isn't an integer
+#(Any positive counting numbers and their negative oposites),
+#It will spit out an error on the conversion of an integer
+#Anything can be converted into a string
+#Any positive counting numbers and their negative oposites, not including when you add ".0" at the end, will work.
